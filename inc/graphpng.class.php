63ed96de433b9f738c92026af69a6e6ca4247b4a
orthagh <orthagh@98255d05-7175-4df8-bd1a-0cbbf92f917d>
2012-04-18 10:44:33 +0000

    add a function to draw antiliased line in graphpng
    git-svn-id: https://forge.indepnet.net/svn/mreporting/trunk@13 98255d05-7175-4df8-bd1a-0cbbf92f917d

--- inc/graphpng.class.php
+++ inc/graphpng.class.php
@@ -185,6 +185,87 @@ class PluginMreportingGraphpng extends PluginMreportingGraph {
      return $new_hex;
    }
 
+   /**
+    * function imageSmoothAlphaLine() - version 1.0
+    * Draws a smooth line with alpha-functionality
+    *
+    * @param   ident    the image to draw on
+    * @param   integer  x1
+    * @param   integer  y1
+    * @param   integer  x2
+    * @param   integer  y2
+    * @param   color    color created by imagecolorallocatealpha
+    *
+    * @access  public
+    *
+    * @author  DASPRiD <d@sprid.de>
+    */
+   function imageSmoothAlphaLine ($image, $x1, $y1, $x2, $y2, $dcol) {
+
+      $rgba = $this->colorHexToRGB($dcol);
+      $r       = $rgba[0];
+      $g       = $rgba[1];
+      $b       = $rgba[2];
+      $alpha   = $rgba[3];
+
+      $icr = $r;
+      $icg = $g;
+      $icb = $b;
+     
+      $m = ($y2 - $y1) / ($x2 - $x1);
+      $b = $y1 - $m * $x1;
+
+      if (abs ($m) <2) {
+         $x = min($x1, $x2);
+         $endx = max($x1, $x2) + 1;
+
+         while ($x < $endx) {
+            $y = $m * $x + $b;
+            $ya = ($y == floor($y) ? 1: $y - floor($y));
+            $yb = ceil($y) - $y;
+      
+            $trgb = ImageColorAt($image, $x, floor($y));
+            $tcr = ($trgb >> 16) & 0xFF;
+            $tcg = ($trgb >> 8) & 0xFF;
+            $tcb = $trgb & 0xFF;
+            imagesetpixel($image, $x, floor($y), imagecolorallocatealpha($image, ($tcr * $ya + $icr * $yb), ($tcg * $ya + $icg * $yb), ($tcb * $ya + $icb * $yb), $alpha));
+     
+            $trgb = ImageColorAt($image, $x, ceil($y));
+            $tcr = ($trgb >> 16) & 0xFF;
+            $tcg = ($trgb >> 8) & 0xFF;
+            $tcb = $trgb & 0xFF;
+            imagesetpixel($image, $x, ceil($y), imagecolorallocatealpha($image, ($tcr * $yb + $icr * $ya), ($tcg * $yb + $icg * $ya), ($tcb * $yb + $icb * $ya), $alpha));
+     
+            $x++;
+         }
+      } else {
+         $y = min($y1, $y2);
+         $endy = max($y1, $y2) + 1;
+
+         while ($y < $endy) {
+            $x = ($y - $b) / $m;
+            $xa = ($x == floor($x) ? 1: $x - floor($x));
+            $xb = ceil($x) - $x;
+     
+            $trgb = ImageColorAt($image, floor($x), $y);
+            $tcr = ($trgb >> 16) & 0xFF;
+            $tcg = ($trgb >> 8) & 0xFF;
+            $tcb = $trgb & 0xFF;
+            imagesetpixel($image, floor($x), $y, imagecolorallocatealpha($image, ($tcr * $xa + $icr * $xb), ($tcg * $xa + $icg * $xb), ($tcb * $xa + $icb * $xb), $alpha));
+     
+            $trgb = ImageColorAt($image, ceil($x), $y);
+            $tcr = ($trgb >> 16) & 0xFF;
+            $tcg = ($trgb >> 8) & 0xFF;
+            $tcb = $trgb & 0xFF;
+            imagesetpixel ($image, ceil($x), $y, imagecolorallocatealpha($image, ($tcr * $xb + $icr * $xa), ($tcg * $xb + $icg * $xa), ($tcb * $xb + $icb * $xa), $alpha));
+     
+            $y ++;
+         }
+      }
+   } // end of 'imageSmoothAlphaLine()' function
+
+
+
    function showHbar($raw_datas, $title, $desc = "", $show_label = 'none', $export = false) {
       $datas = $raw_datas['datas'];
       if (count($datas) <= 0) return false;
@@ -786,10 +867,8 @@ class PluginMreportingGraphpng extends PluginMreportingGraph {
                imagefilledpolygon($image, $points , 4 ,  $alphapalette[$index1]);
             }
 
-
             //trace lines between points
-            imageline($image, $x1, $y1-1, $x2, $y2-1, $palette[$index1]);
-            imageline($image, $x1, $y1, $x2, $y2, $palette[$index1]);
+            $this->imageSmoothAlphaLine ($image, $x1, $y1, $x2, $y2, $palette[$index1]);
 
             //trace dots
             $color_rbg = $this->colorHexToRGB($darkerpalette[$index1]);
