9714704a3082de0e64751a6273f6a1010dd796ec
Xavier Caillaud <tsmr@thetsmr.fr>
2012-04-19 09:03:17 +0000

    [trunk] 0.83 version - to be continued
    git-svn-id: https://forge.indepnet.net/svn/mreporting/trunk@39 98255d05-7175-4df8-bd1a-0cbbf92f917d

--- front/graph.export.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('GLPI_ROOT', '../../..');
-include (GLPI_ROOT."/inc/includes.php");
-header("Content-Type: text/html; charset=UTF-8");
-header_nocache();
-
-$common = new PluginMreportingCommon;
-$common->showGraph($_REQUEST, true);
-?>
--- inc/graph.class.php
+++ inc/graph.class.php
@@ -1,4 +1,32 @@
 <?php
+/*
+ * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
+ -------------------------------------------------------------------------
+ Mreporting plugin for GLPI
+ Copyright (C) 2003-2011 by the mreporting Development Team.
+
+ https://forge.indepnet.net/projects/mreporting
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of mreporting.
+
+ mreporting is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ mreporting is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with mreporting. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+ 
 class PluginMreportingGraph {
    const DEBUG_GRAPH = false;
    protected $width = 700;
@@ -55,7 +83,8 @@ class PluginMreportingGraph {
          echo "</span>";
          echo "<span style='float:right'><b>".$LANG['buttons'][31]."</b> : ";
          echo "&nbsp;<a target='_blank' href='export.php?switchto=csv&$request_string'>CSV</a> /";
-         echo "&nbsp;<a target='_blank' href='export.php?switchto=png&$request_string'>PNG</a>";
+         echo "&nbsp;<a target='_blank' href='export.php?switchto=png&$request_string'>PNG</a> /";
+         echo "&nbsp;<a target='_blank' href='export.php?switchto=odt&$request_string'>ODT</a>";
          echo "</span>";
          echo "<div style='clear:both;'></div>";
          echo "</div>";
@@ -948,3 +977,5 @@ JAVASCRIPT;
 
    }
 }
+
+?>
