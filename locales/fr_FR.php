41171358277214aabd22b0bb1aaed2d727a4ab85
tsmr <tsmr@98255d05-7175-4df8-bd1a-0cbbf92f917d>
2012-05-17 19:42:31 +0000

    Add categories for display all graphs. fix #3589
    git-svn-id: https://forge.indepnet.net/svn/mreporting/trunk@92 98255d05-7175-4df8-bd1a-0cbbf92f917d

--- locales/fr_FR.php
+++ locales/fr_FR.php
@@ -40,19 +40,48 @@ $LANG['plugin_mreporting']["export"][2] = "Données";
 
 $LANG['plugin_mreporting']['Helpdesk']['title'] = "Reporting Helpdesk";
 $LANG['plugin_mreporting']['Helpdesk']['reportHbarTicketNumberByEntity']['title'] = "Barres - Nombre de ticket par entités";
+$LANG['plugin_mreporting']['Helpdesk']['reportHbarTicketNumberByEntity']['category'] = "Par entités";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportPieTicketNumberByEntity']['title'] = "Camembert - Nombre de ticket par entités";
+$LANG['plugin_mreporting']['Helpdesk']['reportPieTicketNumberByEntity']['category'] = "Par entités";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportHgbarTicketNumberByCatAndEntity']['title'] = "Barres groupées - Nombre de ticket par catégories et entités";
-$LANG['plugin_mreporting']['Helpdesk']['reportPieTicketOpenedAndClosed']['title'] = "Camembert - Nombre de tickets ouverts et clôturés";
-$LANG['plugin_mreporting']['Helpdesk']['reportPieTicketOpenedbyStatus']['title'] = "Camembert - Nombre de tickets ouverts par statuts";
+$LANG['plugin_mreporting']['Helpdesk']['reportHgbarTicketNumberByCatAndEntity']['category'] = "Par entités";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportPieTopTenAuthor']['title'] = "Camembert - Top 10 des demandeurs";
+$LANG['plugin_mreporting']['Helpdesk']['reportPieTopTenAuthor']['category'] = "Par demandeurs";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportHgbarOpenTicketNumberByCategoryAndByType']['title'] = "Barres groupées - Nombre de tickets ouverts par catégories et par types";
+$LANG['plugin_mreporting']['Helpdesk']['reportHgbarOpenTicketNumberByCategoryAndByType']['category'] = "Par catégories";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportHgbarCloseTicketNumberByCategoryAndByType']['title'] = "Barres groupées - Nombre de tickets clôturés par catégories et par types";
-$LANG['plugin_mreporting']['Helpdesk']['reportHgbarTicketNumberByService']['title'] = "Barres groupées - Nombre de tickets ouverts et clôturés par services";
+$LANG['plugin_mreporting']['Helpdesk']['reportHgbarCloseTicketNumberByCategoryAndByType']['category'] = "Par catégories";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportHgbarOpenedTicketNumberByCategory']['title'] = "Barres groupées - Nombre de tickets ouverts par catégories et par statuts";
+$LANG['plugin_mreporting']['Helpdesk']['reportHgbarOpenedTicketNumberByCategory']['category'] = "Par catégories";
+
+$LANG['plugin_mreporting']['Helpdesk']['reportHgbarTicketNumberByService']['title'] = "Barres groupées - Nombre de tickets ouverts et clôturés par services";
+$LANG['plugin_mreporting']['Helpdesk']['reportHgbarTicketNumberByService']['category'] = "Par services";
+
+$LANG['plugin_mreporting']['Helpdesk']['reportPieTicketOpenedAndClosed']['title'] = "Camembert - Nombre de tickets ouverts et clôturés";
+$LANG['plugin_mreporting']['Helpdesk']['reportPieTicketOpenedAndClosed']['category'] = "Par tickets";
+
+$LANG['plugin_mreporting']['Helpdesk']['reportPieTicketOpenedbyStatus']['title'] = "Camembert - Nombre de tickets ouverts par statuts";
+$LANG['plugin_mreporting']['Helpdesk']['reportPieTicketOpenedbyStatus']['category'] = "Par tickets";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportAreaNbTicket']['title'] = "Aire - Evolution du nombre de ticket sur la période";
+$LANG['plugin_mreporting']['Helpdesk']['reportAreaNbTicket']['category'] = "Par tickets";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportLineNbTicket']['title'] = "Lignes - Evolution du nombre de ticket sur la période";
+$LANG['plugin_mreporting']['Helpdesk']['reportLineNbTicket']['category'] = "Par tickets";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportGlineNbTicket']['title'] = "Lignes - Evolution du nombre de ticket sur la période (par Statut)";
+$LANG['plugin_mreporting']['Helpdesk']['reportGlineNbTicket']['category'] = "Par tickets";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportGareaNbTicket']['title'] = "Aire - Evolution du nombre de ticket sur la période (par Statut)";
+$LANG['plugin_mreporting']['Helpdesk']['reportGareaNbTicket']['category'] = "Par tickets";
+
 $LANG['plugin_mreporting']['Helpdesk']['reportHgstackbarNbTicket']['title'] = "Barres empilées - Evolution du nombre de ticket sur la période (par Statut)";
+$LANG['plugin_mreporting']['Helpdesk']['reportHgstackbarNbTicket']['category'] = "Par tickets";
 
 ?>
