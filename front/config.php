770bf66b356bf42006177877d20b5e386ca8ee72
tsmr <tsmr@98255d05-7175-4df8-bd1a-0cbbf92f917d>
2012-05-19 12:20:42 +0000

    Add config for graphs. add other options & forgotten files. see #3592 TO be continued
    git-svn-id: https://forge.indepnet.net/svn/mreporting/trunk@98 98255d05-7175-4df8-bd1a-0cbbf92f917d

---
+++ front/config.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
+ -------------------------------------------------------------------------
+ Manufacturersimports plugin for GLPI
+ Copyright (C) 2003-2011 by the Manufacturersimports Development Team.
+
+ https://forge.indepnet.net/projects/manufacturersimports
+ -------------------------------------------------------------------------
+
+ LICENSE
+		
+ This file is part of Manufacturersimports.
+
+ Manufacturersimports is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Manufacturersimports is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Manufacturersimports. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+if (!defined('GLPI_ROOT')) {
+	define('GLPI_ROOT', '../../..');
+	include (GLPI_ROOT . "/inc/includes.php");
+}
+
+Session::checkRight("config","w");
+
+$plugin = new Plugin();
+	if ($plugin->isActivated("mreporting")) {
+
+      Html::header($LANG['common'][12],'',"plugins","mreporting","config");
+      
+      Search::show("PluginMreportingConfig");
+
+	} else {
+		Html::header($LANG['common'][12],'',"config","plugins");
+		echo "<div align='center'><br><br><img src=\"".$CFG_GLPI["root_doc"]."/pics/warning.png\" alt=\"warning\"><br><br>";
+		echo "<b>Please activate the plugin</b></div>";
+	}
+
+	Html::footer();
+
+?>
--- inc/graph.class.php
+++ inc/graph.class.php
@@ -50,6 +50,8 @@ class PluginMreportingGraph {
          
       echo "<img src='../pics/chart-$prev_function.png' class='title_pics' />";
       echo $options['title'];
+      echo "&nbsp;<a href='config.form.php?rand=".$rand."' target='_blank'>";
+      echo "<img src='../pics/cog_edit.png' class='title_pics'/></a>";
       echo "</div>";
       if (!empty($options['desc'])) {
          echo "<div class='graph_desc'>".$options['desc']."</div>";
@@ -214,10 +216,10 @@ class PluginMreportingGraph {
       
       $datas = $raw_datas['datas'];
       
-      $show_label = 'never';
-      $config = new PluginMreportingConfig();
-      if ($config->getFromDBByRand($rand)) {
-         $show_label = $config->fields['show_label'];
+      $configs = PluginMreportingConfig::initConfigParams($rand);
+      
+      foreach ($configs as $k => $v) {
+         $$k=$v;
       }
       
       $this->initDatasSimple($datas, $unit);
@@ -360,10 +362,10 @@ JAVASCRIPT;
       
       $datas = $raw_datas['datas'];
       
-      $show_label = 'never';
-      $config = new PluginMreportingConfig();
-      if ($config->getFromDBByRand($rand)) {
-         $show_label = $config->fields['show_label'];
+      $configs = PluginMreportingConfig::initConfigParams($rand);
+      
+      foreach ($configs as $k => $v) {
+         $$k=$v;
       }
       
       $this->initDatasSimple($datas, $unit);
@@ -501,12 +503,10 @@ JAVASCRIPT;
       
       $datas = $raw_datas['datas'];
       
-      $flip_data = false;
-      $show_label = 'never';
-      $config = new PluginMreportingConfig();
-      if ($config->getFromDBByRand($rand)) {
-         $flip_data = $config->fields['flip_data'];
-         $show_label = $config->fields['show_label'];
+      $configs = PluginMreportingConfig::initConfigParams($rand);
+      
+      foreach ($configs as $k => $v) {
+         $$k=$v;
       }
       
       $labels2 = $raw_datas['labels2'];
@@ -667,12 +667,10 @@ JAVASCRIPT;
       
       $datas = $raw_datas['datas'];
       
-      $flip_data = false;
-      $show_label = 'never';
-      $config = new PluginMreportingConfig();
-      if ($config->getFromDBByRand($rand)) {
-         $flip_data = $config->fields['flip_data'];
-         $show_label = $config->fields['show_label'];
+      $configs = PluginMreportingConfig::initConfigParams($rand);
+      
+      foreach ($configs as $k => $v) {
+         $$k=$v;
       }
 
       $labels2 = $raw_datas['labels2'];
@@ -807,14 +805,10 @@ JAVASCRIPT;
       
       $datas = $raw_datas['datas'];
       
-      $area = false;
-      $spline = false;
-      $show_label = 'never';
-      $config = new PluginMreportingConfig();
-      if ($config->getFromDBByRand($rand)) {
-         $area = $config->fields['show_area'];
-         $spline = $config->fields['spline'];
-         $show_label = $config->fields['show_label'];
+      $configs = PluginMreportingConfig::initConfigParams($rand);
+      
+      foreach ($configs as $k => $v) {
+         $$k=$v;
       }
 
       $this->initDatasSimple($datas, $unit);
@@ -1011,16 +1005,10 @@ JAVASCRIPT;
       
       $datas = $raw_datas['datas'];
       
-      $area = false;
-      $flip_data = false;
-      $spline = false;
-      $show_label = 'never';
-      $config = new PluginMreportingConfig();
-      if ($config->getFromDBByRand($rand)) {
-         $flip_data = $config->fields['flip_data'];
-         $area = $config->fields['show_area'];
-         $spline = $config->fields['spline'];
-         $show_label = $config->fields['show_label'];
+      $configs = PluginMreportingConfig::initConfigParams($rand);
+      
+      foreach ($configs as $k => $v) {
+         $$k=$v;
       }
       
       $labels2 = $raw_datas['labels2'];
