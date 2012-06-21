b0b7ae19277c6155f521b5ffc08d72d17a7d6af0
Xavier Caillaud <tsmr@thetsmr.fr>
2012-06-21 07:56:17 +0000

    Update headers
    git-svn-id: https://forge.indepnet.net/svn/mreporting/trunk@211 98255d05-7175-4df8-bd1a-0cbbf92f917d

--- ajax/common.tabs.php
+++ ajax/common.tabs.php
@@ -2,30 +2,30 @@
 /*
  * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
  -------------------------------------------------------------------------
- Manageentities plugin for GLPI
- Copyright (C) 2003-2011 by the Manageentities Development Team.
+ Mreporting plugin for GLPI
+ Copyright (C) 2003-2011 by the mreporting Development Team.
 
- https://forge.indepnet.net/projects/manageentities
+ https://forge.indepnet.net/projects/mreporting
  -------------------------------------------------------------------------
 
  LICENSE
-		
- This file is part of Manageentities.
 
- Manageentities is free software; you can redistribute it and/or modify
+ This file is part of mreporting.
+
+ mreporting is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- Manageentities is distributed in the hope that it will be useful,
+ mreporting is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Manageentities. If not, see <http://www.gnu.org/licenses/>.
+ along with mreporting. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
-// */
+ */
 
 define('GLPI_ROOT', '../../..');
 include (GLPI_ROOT."/inc/includes.php");
