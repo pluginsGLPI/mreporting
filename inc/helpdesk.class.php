55115623f39f407dfc5683fb193905b20461c479
orthagh <orthagh@98255d05-7175-4df8-bd1a-0cbbf92f917d>
2012-08-06 14:11:59 +0000

    fix tooltip length in sunburst
    git-svn-id: https://forge.indepnet.net/svn/mreporting/trunk@228 98255d05-7175-4df8-bd1a-0cbbf92f917d

--- inc/helpdesk.class.php
+++ inc/helpdesk.class.php
@@ -638,7 +638,8 @@ class PluginMreportingHelpdesk Extends PluginMreportingBaseclass {
            'key1' => array('key1.1' => 12, 'key1.2' => 25, 'key1.3' => 43), 
            'key2' => array('key2.1' => array(
                "2.3.1"=>10,"2.3.2"=>8,"2.3.3" =>17)/*25*/, 'key2.2' => 18, 'key2.3' => 25),
-           'key3' => array('key3.1' => 12, 'key3.2' => 25, 'key3.3' => 43)
+           'key3' => array('key3.1' => 12, 'key3.2' => 25, 'key3.3' => 43), 
+           'very looooooooooooooooooooooooooooooooooooooooooooog key' => 54
          );
       return $datas;
    }
