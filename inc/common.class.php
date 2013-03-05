9a76b84cc591614e89feec7a63b2372e52f867f3
Xavier Caillaud <tsmr@thetsmr.fr>
2013-03-05 11:05:58 +0000

    The odt export don't load data on display array when use simple datas. fix #4252
    git-svn-id: https://forge.indepnet.net/svn/mreporting/trunk@282 98255d05-7175-4df8-bd1a-0cbbf92f917d

--- inc/common.class.php
+++ inc/common.class.php
@@ -1247,6 +1247,8 @@ class PluginMreportingCommon extends CommonDBTM {
                      if (isset($labels2[$key]))
                         $types[$key][$k] = $val;
                   }
+               } else {
+                  $types[$k] = $v;
                }
             }
             
