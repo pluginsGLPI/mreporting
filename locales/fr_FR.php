c4f380014d69c77a1ac6c369a45934e5272ab17e
Walid <wnouh@teclib.com>
2014-09-29 10:59:26 +0200

    Add inventory reports
    Conflicts:
    	locales/fr_FR.php

--- locales/fr_FR.php
+++ locales/fr_FR.php
@@ -132,4 +132,65 @@ $LANG['plugin_mreporting']['Helpdesk']['reportVstackbarTicketStatusByTechnician'
 $LANG['plugin_mreporting']['Helpdesk']['reportHbarTicketNumberByLocation']['title'] = "Nombre de ticket par lieu des demandeurs";
 $LANG['plugin_mreporting']['Helpdesk']['reportHbarTicketNumberByLocation']['desc'] = "Barres";
 $LANG['plugin_mreporting']['Helpdesk']['reportHbarTicketNumberByLocation']['category'] = "Par demandeurs";
-?>
+
+$LANG['plugin_mreporting']['Inventory']['title'] = "Reporting inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByOS']['title'] = "Ordinateurs par OS";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByOS']['desc'] = "Barres";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByOS']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByOS']['title'] = "Ordinateurs par OS";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByOS']['desc'] = "Camenbert";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByOS']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByFabricant']['title'] = "Ordinateurs par fabricant";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByFabricant']['desc'] = "Barres";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByFabricant']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByFabricant']['title'] = "Ordinateurs par fabricant";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByFabricant']['desc'] = "Camembert";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByFabricant']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByType']['title'] = "Ordinateurs par type";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByType']['desc'] = "Barres";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByType']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByType']['title'] = "Ordinateurs par type";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByType']['desc'] = "Camembert";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByType']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByDomain']['title'] = "Ordinateurs par domaine (mono et multi-domaine)";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByDomain']['desc'] = "Barres";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByDomain']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportHbarWindows']['title'] = "Répartition des versions de Windows dans le parc";
+$LANG['plugin_mreporting']['Inventory']['reportHbarWindows']['desc'] = "Barres";
+$LANG['plugin_mreporting']['Inventory']['reportHbarWindows']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportHbarLinux']['title'] = "Répartition des distributions de Linux dans le parc";
+$LANG['plugin_mreporting']['Inventory']['reportHbarLinux']['desc'] = "Barres";
+$LANG['plugin_mreporting']['Inventory']['reportHbarLinux']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByAge']['title'] = "Ordinateurs par &acirc;ge";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByAge']['desc'] = "Barres";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByAge']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByAge']['title'] = "Ordinateurs par &acirc;ge";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByAge']['desc'] = "Camembert";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByAge']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportHbarFusionInventory']['title'] = "Répartition version agent FusionInventory";
+$LANG['plugin_mreporting']['Inventory']['reportHbarFusionInventory']['desc'] = "Barres";
+$LANG['plugin_mreporting']['Inventory']['reportHbarFusionInventory']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['Inventory']['reportPieFusionInventory']['title'] = "Répartition version agent FusionInventory";
+$LANG['plugin_mreporting']['Inventory']['reportPieFusionInventory']['desc'] = "Camembert";
+$LANG['plugin_mreporting']['Inventory']['reportPieFusionInventory']['category'] = "Inventaire";
+
+$LANG['plugin_mreporting']['selector']["status"]   = "Status";
+
+$LANG['plugin_mreporting']['selector']["period"][0] = "Période";
+$LANG['plugin_mreporting']['selector']["period"][1] = "Jour";
+$LANG['plugin_mreporting']['selector']["period"][2] = "Semaine";
+$LANG['plugin_mreporting']['selector']["period"][3] = "Mois";
+$LANG['plugin_mreporting']['selector']["period"][4] = "Année";
