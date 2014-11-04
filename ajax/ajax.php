f327ba39c5fe830cd2044c76dd287fde56da4224
Stanislas <skita@teclib.com>
2014-11-04 08:42:07 +0100

    update file

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -2,36 +2,17 @@
 
 include('../../../inc/includes.php');
 
-if (isset($_POST['action'])) {
-
-    global $CFG_GLPI;
-
-    switch ($_POST['action']) {
-
-        //TEST DE LA CONNECTIVITE A MANTIS CONNECT
-        case 'updateReportProfile':
-
-            $id = $_POST['id'];
-            $right = $_POST['right'];
-
-            $reportProfiles = new PluginMreportingProfile();
-            $res =  $reportProfiles->update(array("id" => $id , "right" => $right));
-
-            if($res) echo "<img src='".$CFG_GLPI['root_doc']."/plugins/mreporting/pics/check24.png' />";
-            else echo "<img src='".$CFG_GLPI['root_doc']."/plugins/mreporting/pics/cross24.png'/>";
-
-            break;
+global $CFG_GLPI;
 
 
+$id = $_POST['id'];
+$right = $_POST['right'];
 
+$reportProfiles = new PluginMreportingProfile();
+$res =  $reportProfiles->update(array("id" => $id , "right" => $right));
 
+if($res) echo "<img src='".$CFG_GLPI['root_doc']."/plugins/mreporting/pics/check24.png' />";
+else echo "<img src='".$CFG_GLPI['root_doc']."/plugins/mreporting/pics/cross24.png'/>";
 
-        default:
-            echo 0;
-    }
-
-} else {
-    echo 0;
-}
 
 
