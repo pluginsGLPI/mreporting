5c609d3646ca79d8b8f63e94b3dff9520bcc26a6
Walid <wnouh@teclib.com>
2014-12-03 15:01:44 +0100

    Fix english locales once again

--- locales/teclib_en_GB.php
+++ locales/teclib_en_GB.php
@@ -31,27 +31,27 @@ global $LANG;
 
 $LANG['plugin_mreporting']['Inventory']['title'] = "Inventory reports";
 
-$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByOS']['title'] = "Computers by OS";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByOS']['title'] = "Computers per OS";
 $LANG['plugin_mreporting']['Inventory']['reportHbarComputersByOS']['desc'] = "Bars";
 $LANG['plugin_mreporting']['Inventory']['reportHbarComputersByOS']['category'] = "Inventory";
 
-$LANG['plugin_mreporting']['Inventory']['reportPieComputersByOS']['title'] = "Computers by OS";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByOS']['title'] = "Computers per OS";
 $LANG['plugin_mreporting']['Inventory']['reportPieComputersByOS']['desc'] = "Camenbert";
 $LANG['plugin_mreporting']['Inventory']['reportPieComputersByOS']['category'] = "Inventory";
 
-$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByFabricant']['title'] = "Computers by manufacturer";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByFabricant']['title'] = "Computers per manufacturer";
 $LANG['plugin_mreporting']['Inventory']['reportHbarComputersByFabricant']['desc'] = "Bars";
 $LANG['plugin_mreporting']['Inventory']['reportHbarComputersByFabricant']['category'] = "Inventory";
 
-$LANG['plugin_mreporting']['Inventory']['reportPieComputersByFabricant']['title'] = "Computers by manufacturer";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByFabricant']['title'] = "Computers per manufacturer";
 $LANG['plugin_mreporting']['Inventory']['reportPieComputersByFabricant']['desc'] = "Pie";
 $LANG['plugin_mreporting']['Inventory']['reportPieComputersByFabricant']['category'] = "Inventory";
 
-$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByType']['title'] = "Computers by type";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByType']['title'] = "Computers per type";
 $LANG['plugin_mreporting']['Inventory']['reportHbarComputersByType']['desc'] = "Bars";
 $LANG['plugin_mreporting']['Inventory']['reportHbarComputersByType']['category'] = "Inventory";
 
-$LANG['plugin_mreporting']['Inventory']['reportPieComputersByType']['title'] = "Computers by type";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByType']['title'] = "Computers per type";
 $LANG['plugin_mreporting']['Inventory']['reportPieComputersByType']['desc'] = "Pie";
 $LANG['plugin_mreporting']['Inventory']['reportPieComputersByType']['category'] = "Inventory";
 
@@ -63,11 +63,11 @@ $LANG['plugin_mreporting']['Inventory']['reportHbarLinux']['title'] = "Linux dis
 $LANG['plugin_mreporting']['Inventory']['reportHbarLinux']['desc'] = "Bars";
 $LANG['plugin_mreporting']['Inventory']['reportHbarLinux']['category'] = "Inventory";
 
-$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByAge']['title'] = "Computer by age";
+$LANG['plugin_mreporting']['Inventory']['reportHbarComputersByAge']['title'] = "Computer per age";
 $LANG['plugin_mreporting']['Inventory']['reportHbarComputersByAge']['desc'] = "Bars";
 $LANG['plugin_mreporting']['Inventory']['reportHbarComputersByAge']['category'] = "Inventory";
 
-$LANG['plugin_mreporting']['Inventory']['reportPieComputersByAge']['title'] = "Computer by age";
+$LANG['plugin_mreporting']['Inventory']['reportPieComputersByAge']['title'] = "Computer per age";
 $LANG['plugin_mreporting']['Inventory']['reportPieComputersByAge']['desc'] = "Pie";
 $LANG['plugin_mreporting']['Inventory']['reportPieComputersByAge']['category'] = "Inventory";
 
@@ -79,7 +79,7 @@ $LANG['plugin_mreporting']['Inventory']['reportPieFusionInventory']['title'] = "
 $LANG['plugin_mreporting']['Inventory']['reportPieFusionInventory']['desc'] = "Pie";
 $LANG['plugin_mreporting']['Inventory']['reportPieFusionInventory']['category'] = "Inventory";
 
-$LANG['plugin_mreporting']['Inventory']['reportHbarMonitors']['title'] = "Screens by computer distribution";
+$LANG['plugin_mreporting']['Inventory']['reportHbarMonitors']['title'] = "Screens per computer distribution";
 $LANG['plugin_mreporting']['Inventory']['reportHbarMonitors']['desc'] = "Bars";
 $LANG['plugin_mreporting']['Inventory']['reportHbarMonitors']['category'] = "Inventory";
 
