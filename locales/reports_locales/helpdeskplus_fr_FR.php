eb3a01ce551ac1ae9fdc288586953f060111ccc7
Alexandre Delaunay <adelaunay@teclib.com>
2014-12-18 16:42:39 +0100

    explode locales files
    Conflicts:
    	locales/en_GB.php
    	locales/fr_FR.php
    	locales/reports_locales/teclib_en_GB.php
    	locales/reports_locales/teclib_fr_FR.php

---
+++ locales/reports_locales/helpdeskplus_fr_FR.php
@@ -0,0 +1,43 @@
+<?php
+global $LANG;
+
+$LANG['plugin_mreporting']['Helpdeskplus']['title'] = 'Reporting Helpdesk Avancé';
+
+$LANG['plugin_mreporting']['Helpdeskplus']['reportGlineBacklogs']['title']            = 'Backlog';
+$LANG['plugin_mreporting']['Helpdeskplus']['reportGlineBacklogs']['desc']             = 'Ce rapport affiche le nombre de ticket groupé par la période selectionnée (jour, semaine, ...).<br> Il ajoute par ailleurs une nouvelle ligne nommée "Backlogs" permettant de visualiser <i>l\'en cours</i> des tickets à une date donnée';
+
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarLifetime']['title']        = 'Ancienneté des tickets';
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarLifetime']['desc']         = 'Ce rapport affiche des barres indiquant le nombre de tickets pour la période selectionnée (jour, semaine, ...).<br>
+Les tickets sont affichés dans leurs statuts courants. Par exemple, les blocs \'Nouveau\' affiche les tickets dans un statut nouveau à la date d\'aujourd\'hui.';
+
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarTicketsgroups']['title']   = 'Quantitatif par groupes';
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarTicketsgroups']['desc']    = 'Ce rapport affiche le nombre de tickets ouverts entre les dates selectionnées par leur groupe d\'attribution';
+
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarTicketstech']['title']     = 'Quantitatif par technicien';
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarTicketstech']['desc']      = 'Ce rapport affiche le nombre de tickets ouverts entre les dates selectionnées groupés par techniciens.<br>
+<b>Vous devez selectionner au préalable un groupe de technicien pour afficher les données.</b>';
+
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarWorkflow']['title']        = 'Orientation des files';
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarWorkflow']['desc']         = 'Nombre de tickets pour chaque groupe d\'arrivée en fonction d\'un groupe de départ.<br />
+                                                                                       - Sélectionnez un groupe demandeur pour afficher le nombre de ticket pour les groupes attribués.<br />
+                                                                                       - Sélectionnez un groupe chargé du ticket pour afficher le nombre de ticket pour les groupes demandeurs.<br />
+                                                                                       <b>Vous ne pouvez sélectionner qu\'un critère à la fois !</b>';
+
+$LANG['plugin_mreporting']['Helpdeskplus']['reportHbarTopcategory']['title']          = 'TOP catégories';
+$LANG['plugin_mreporting']['Helpdeskplus']['reportHbarTopcategory']['desc']           = 'Nombre croissant de ticket affiché par catégorie. <br>
+Il est possible limiter le nombre de catégories affichées !';
+
+$LANG['plugin_mreporting']['Helpdeskplus']['reportHbarTopapplicant']['title']         = 'TOP groupes demandeurs';
+$LANG['plugin_mreporting']['Helpdeskplus']['reportHbarTopapplicant']['desc']          = 'Nombre croissant de ticket affiché par groupe demandeur. <br>
+Il est possible limiter le nombre de groupes affichés !';
+
+$LANG['plugin_mreporting']['Helpdeskplus']['backlogs']        = 'Backlogs';
+$LANG['plugin_mreporting']['Helpdeskplus']['opened']          = 'Ouverts';
+$LANG['plugin_mreporting']['Helpdeskplus']['period']          = 'Période';
+$LANG['plugin_mreporting']['Helpdeskplus']['backlogstatus']   = 'Statuts à afficher';
+
+
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarGroupChange']['title'] = "Nombre de changement de groupe";
+$LANG['plugin_mreporting']['Helpdeskplus']['reportVstackbarGroupChange']['desc'] = "Le graphique suivant affiche les nombres de tickets classé par nombre de changements.<br>
+En ordonnée est affiché le nombre de ticket.<br>
+En abscisse, le nombre de changements.";
---
+++ locales/reports_locales/other_en_GB.php
@@ -0,0 +1,7 @@
+<?php
+global $LANG;
+
+$LANG['plugin_mreporting']['Other']['title'] = "Other";
+$LANG['plugin_mreporting']['Other']['reportHbarLogs']['title'] = "Logs distribution";
+$LANG['plugin_mreporting']['Other']['reportHbarLogs']['desc'] = "Bars";
+$LANG['plugin_mreporting']['Other']['reportHbarLogs']['category'] = "Logs";
