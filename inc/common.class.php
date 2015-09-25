728b96ee3a0eff3434d96cf7a65a684761062304
Manu1400 <Manu1400@users.noreply.github.com>
2015-09-25 12:14:20 +0200

    Fix saving bookmark(s) : save a exist itemtype
    Don't save '0' as itemtype

--- inc/common.class.php
+++ inc/common.class.php
@@ -1643,7 +1643,7 @@ class PluginMreportingCommon extends CommonDBTM {
       }
       $_SERVER['REQUEST_URI'] .= "&date1".$randname."=".$date1;
       $_SERVER['REQUEST_URI'] .= "&date2".$randname."=".$date2;
-      Bookmark::showSaveButton(Bookmark::URI);
+      Bookmark::showSaveButton(Bookmark::URI, __CLASS__);
 
       //If there's no selector for the report, there's no need for a reset button !
       if ($has_selector) {
