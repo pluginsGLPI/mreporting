efcf4e01337dd7cf0e3bfe6d1e37c2309979a108
Emmanuel Haguet <ehaguet@teclib.com>
2015-10-26 15:54:16 +0100

    Fix/Best export SVG : add xmlns in SVG for web browser

--- front/export_svg.php
+++ front/export_svg.php
@@ -1,5 +1,4 @@
 <?php
-
 include ("../../../inc/includes.php");
 
 header("Expires: Mon, 26 Nov 1962 00:00:00 GMT");
@@ -8,4 +7,6 @@ header('Cache-control: private, must-revalidate'); /// IE BUG + SSL
 header("Content-disposition: attachment; filename=export.svg");
 header("Content-type: image/svg+xml");
 
-echo Toolbox::stripslashes_deep(html_entity_decode($_REQUEST['svg_content']));
+$svg_content = Toolbox::stripslashes_deep(html_entity_decode($_REQUEST['svg_content']));
+
+echo str_replace("<svg ", '<svg version="1.1" baseProfile="full" xmlns="http://www.w3.org/2000/svg" ', $svg_content);
