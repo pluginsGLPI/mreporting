62fad8fb551c34c8ab63a54778b9f9b8e25bd534
François Legastelois <flegastelois@teclib.com>
2016-09-05 16:25:06 +0200

    add graph for plugin Tag

---
+++ locales/reports_locales/tag_en_GB.php
@@ -0,0 +1,12 @@
+<?php
+global $LANG;
+
+$LANG['plugin_mreporting']['Tag']['title'] = "Plugin TAG";
+
+$LANG['plugin_mreporting']['Tag']['reportPieTag']['title'] = "Number of tag";
+$LANG['plugin_mreporting']['Tag']['reportPieTag']['desc'] = "Pie";
+$LANG['plugin_mreporting']['Tag']['reportPieTag']['category'] = "";
+
+$LANG['plugin_mreporting']['Tag']['reportPieTagOnTicket']['title'] = "Number of tag on tickets";
+$LANG['plugin_mreporting']['Tag']['reportPieTagOnTicket']['desc'] = "Pie";
+$LANG['plugin_mreporting']['Tag']['reportPieTagOnTicket']['category'] = "";
