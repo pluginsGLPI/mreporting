417b6f76b76c7b40d85dc7cc7d356649eef04844
François Legastelois <flegastelois@users.noreply.github.com>
2016-10-24 10:15:03 +0200

    Add graph for plugin Tag; fixes 96

---
+++ locales/reports_locales/tag_fr_FR.php
@@ -0,0 +1,20 @@
+<?php
+global $LANG;
+
+$LANG['plugin_mreporting']['Tag'] = array(
+
+   'title' => "Plugin TAG",
+
+   'reportPieTag' => array(
+      'title'    => "Nombre d'utilisation des TAG",
+      'desc'     => "Camembert",
+      'category' => "Pour tous les objets GLPi",
+   ),
+
+   'reportPieTagOnTicket' => array(
+      'title'    => "Nombre d'utilisation des TAG",
+      'desc'     => "Camembert",
+      'category' => "Pour les tickets",
+   )
+
+);
