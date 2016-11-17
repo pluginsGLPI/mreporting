58c8c410df5c38d737a0f08b6e163451e132a17b
François Legastelois <flegastelois@teclib.com>
2016-11-17 15:36:50 +0100

    fix #78 review header function for helpdesk dashboard interface

--- front/dashboard.form.php
+++ front/dashboard.form.php
@@ -11,29 +11,41 @@ Session::checkLoginUser();
 
 if (isset($_POST['saveConfig'])) {
 
+   PluginMreportingCommon::saveSelectors($_POST['f_name']);
 
-    PluginMreportingCommon::saveSelectors($_POST['f_name']);
+   $_REQUEST['f_name'] = $_POST['f_name'];
+   $_REQUEST['short_classname'] = $_POST['short_classname'];
+   PluginMreportingCommon::getSelectorValuesByUser();
 
-    $_REQUEST['f_name'] = $_POST['f_name'];
-    $_REQUEST['short_classname'] = $_POST['short_classname'];
-    PluginMreportingCommon::getSelectorValuesByUser();
-
-    Html::back();
-
+   Html::back();
 
 } else if (isset($_POST['addReports'])) {
 
-    $dashboard = new PluginMreportingDashboard();
-    $post = array('users_id' => $_SESSION['glpiID'], 'reports_id' => $_POST['report']);
-    $dashboard->add($post);
+   $dashboard = new PluginMreportingDashboard();
+   $post = array('users_id' => $_SESSION['glpiID'], 'reports_id' => $_POST['report']);
+   $dashboard->add($post);
 
-
-    Html::back();
+   Html::back();
 
 } else {
-    Html::header(__("More Reporting", 'mreporting'), '' ,'tools', 'PluginMreportingCommon', 'dashboard');
-    $dashboard = new PluginMreportingDashboard();
-    $dashboard->showDashBoard();
 
-    Html::footer();
+   if ($_SESSION['glpiactiveprofile']['interface'] == 'helpdesk') {
+      Html::helpHeader(
+         __("More Reporting", 'mreporting'),
+         $_SERVER['PHP_SELF']
+      );
+   } else {
+      Html::header(
+         __("More Reporting", 'mreporting'),
+         $_SERVER['PHP_SELF'],
+         'tools',
+         'PluginMreportingCommon',
+         'dashboard'
+      );
+   }
+
+   $dashboard = new PluginMreportingDashboard();
+   $dashboard->showDashBoard();
+
+   Html::footer();
 }
