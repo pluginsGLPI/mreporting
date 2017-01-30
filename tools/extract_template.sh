17413b9329c3a324f38ac9c552dbb0226e030656
Johan Cwiklinski <jcwiklinski@teclib.com>
2017-01-30 17:02:18 +0100

    Move JS in js subdir

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -6,7 +6,7 @@ pushd $DIR > /dev/null
 NAME='MReporting'
 POTFILE=${NAME,,}.pot
 
-PHP_SOURCES=`find ./ -name \*.php -not -path "./vendor/*" ! -not -path "./lib/*"`
+PHP_SOURCES=`find ./ -name \*.php -not -path "./vendor/*" -not -path "./lib/*"`
 
 if [ ! -d "locales" ]; then
     mkdir locales
