626473a6331962a92f7ed30bdf7afde17994a0ea
Eskiso <eSkiSo@users.noreply.github.com>
2017-02-23 19:13:41 +0000

    Portuguese (Portugal) translation
    Create helpdesk_pt_PT.php
    Fixed minor translation
    Created helpdeskplus_pt_PT.php
    Create inventory_pt_PT.php
    Create other_pt_PT.php

---
+++ locales/reports_locales/other_pt_PT.php
@@ -0,0 +1,10 @@
+<?php
+global $LANG;
+$LANG['plugin_mreporting']['Other'] = array(
+   'title' => "Outros",
+   'reportHbarLogs' => array(
+      'title'    => "Distribuição de Registros",
+      'desc'     => "Barras",
+      'category' => "Registros",
+   )
+);
