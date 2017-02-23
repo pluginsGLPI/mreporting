21c22bd57e1c9085273ebdc260b8751f9f8325b5
Eskiso <eSkiSo@users.noreply.github.com>
2017-02-23 19:13:41 +0000

    Create helpdesk_pt_PT.php
    Portuguese (Portugal) translation

---
+++ locales/reports_locales/helpdesk_pt_PT.php
@@ -0,0 +1,95 @@
+<?php
+global $LANG;
+$LANG['plugin_mreporting']['Helpdesk'] = array(
+   'title' => "Helpdesk",
+   'reportHbarTicketNumberByEntity' => array(
+      'title'    => "Número de indidências por Entidade",
+      'desc'     => "Barras",
+      'category' => "Por entidade",
+   ),
+   'reportPieTicketNumberByEntity' => array(
+      'title'    => "Número de indidências por Entidade",
+      'desc'     => "Circular",
+      'category' => "Por entidade",
+   ),
+   'reportHgbarTicketNumberByCatAndEntity' => array(
+      'title'    => "Número de indidências por Categoria e Entidade",
+      'desc'     => "Barras Agrupadas",
+      'category' => "Por entidade",
+   ),
+   'reportPieTopTenAuthor' => array(
+      'title'    => "Top 10 requerentes",
+      'desc'     => "Circular",
+      'category' => "Por requerente",
+   ),
+   'reportHgbarOpenTicketNumberByCategoryAndByType' => array(
+      'title'    => "Número de indidências abertas por Categoria e Tipo",
+      'desc'     => "Barras Agrupadas",
+      'category' => "Por categoria",
+   ),
+   'reportHgbarCloseTicketNumberByCategoryAndByType' => array(
+      'title'    => "Número de indidências fechadas por Categoria e Tipo",
+      'desc'     => "Barras Agrupadas",
+      'category' => "Por categoria",
+   ),
+   'reportHgbarOpenedTicketNumberByCategory' => array(
+      'title'    => "Número de indidências abertas por Categoria e Estado",
+      'desc'     => "Barras Agrupadas",
+      'category' => "Por categoria",
+   ),
+   'reportHgbarTicketNumberByService' => array(
+      'title'    => "Número de indidências abertas e fechadas por serviço",
+      'desc'     => "Barras Agrupadas",
+      'category' => "Por serviço",
+   ),
+   'reportPieTicketOpenedAndClosed' => array(
+      'title'    => "Número de indidências abertas e fechadas",
+      'desc'     => "Circular",
+      'category' => "Por indidência",
+   ),
+   'reportPieTicketOpenedbyStatus' => array(
+      'title'    => "Número de indidências abertas por estado",
+      'desc'     => "Circular",
+      'category' => "Por indidência",
+   ),
+   'reportAreaNbTicket' => array(
+      'title'    => "Evolução do número de indidências num perido",
+      'desc'     => "Area",
+      'category' => "Por indidência",
+   ),
+   'reportLineNbTicket' => array(
+      'title'    => "Evolução do número de indidências num perido",
+      'desc'     => "Linhas",
+      'category' => "Por indidência",
+   ),
+   'reportGlineNbTicket' => array(
+      'title'    => "Evolução do número de indidências num perido (por estado)",
+      'desc'     => "Linhas",
+      'category' => "Por indidência",
+   ),
+   'reportGareaNbTicket' => array(
+      'title'    => "Evolução do número de indidências num perido (por estado)",
+      'desc'     => "Area",
+      'category' => "Por indidência",
+   ),
+   'reportVstackbarNbTicket' => array(
+      'title'    => "Evolução do número de indidências num perido (por estado)",
+      'desc'     => "Barras Empilhadas",
+      'category' => "Por indidência",
+   ),
+   'reportSunburstTicketByCategories' => array(
+      'title'    => "Distribuição de indidências por categoria e categorias filho",
+      'desc'     => "Donut",
+      'category' => "Por categoria",
+   ),
+   'reportVstackbarTicketStatusByTechnician' => array(
+      'title'    => "Número de indidências por estado e técnico",
+      'desc'     => "Barras Empilhadas",
+      'category' => "Por indidência",
+   ),
+   'reportHbarTicketNumberByLocation' => array(
+      'title'    => "Número de indidências por requerente e local",
+      'desc'     => "Barras",
+      'category' => "Por requerente",
+   ),
+);
