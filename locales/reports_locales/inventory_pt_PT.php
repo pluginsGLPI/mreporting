2804b052f4192d56d0756ec7e06b8762caa3fa75
Eskiso <eSkiSo@users.noreply.github.com>
2017-02-23 19:32:41 +0000

    Create inventory_pt_PT.php

---
+++ locales/reports_locales/inventory_pt_PT.php
@@ -0,0 +1,100 @@
+<?php
+global $LANG;
+$LANG['plugin_mreporting']['Inventory'] = array(
+   'title' => "Inventário",
+   'reportHbarComputersByOS' => array(
+      'title'    => "Computadores por SO",
+      'desc'     => "Barras",
+      'category' => "SO",
+   ),
+   'reportPieComputersByOS' => array(
+      'title'    => "Computadores por SO",
+      'desc'     => "Circular",
+      'category' => "SO",
+   ),
+   'reportHbarComputersByFabricant' => array(
+      'title'    => "Computadores por fabricante",
+      'desc'     => "Barras",
+      'category' => "Fabricante",
+   ),
+   'reportPieComputersByFabricant' => array(
+      'title'    => "Computadores por fabricante",
+      'desc'     => "Circular",
+      'category' => "Fabricante",
+   ),
+   'reportHbarComputersByType' => array(
+      'title'    => "Computadores por tipo",
+      'desc'     => "Barras",
+      'category' => "Tipo",
+   ),
+   'reportPieComputersByType' => array(
+      'title'    => "Computadores por tipo",
+      'desc'     => "Circular",
+      'category' => "Tipo",
+   ),
+   'reportHbarComputersByStatus' => array(
+      'title'    => "Computadores por estado",
+      'desc'     => "Barras",
+      'category' => "Estado",
+   ),
+   'reportHbarPrintersByStatus' => array(
+      'title'    => "Impressoras por estado",
+      'desc'     => "Barras",
+      'category' => "Estado",
+   ),
+   'reportHbarWindows' => array(
+      'title'    => "Distribuição Windows",
+      'desc'     => "Barras",
+      'category' => "SO",
+   ),
+   'reportHbarLinux' => array(
+      'title'    => "Distribuição Linux detalhe",
+      'desc'     => "Barras",
+      'category' => "SO",
+   ),
+   'reportHbarLinuxDistro' => array(
+      'title'    => "Distribuição Linux detalhe",
+      'desc'     => "Barras",
+      'category' => "SO",
+   ),
+   'reportHbarMac' => array(
+      'title'    => "Distribuições Mac OS X detalhes",
+      'desc'     => "Barras",
+      'category' => "SO",
+   ),
+   'reportHbarMacFamily' => array(
+      'title'    => "Versão MAC OS X visão geral",
+      'desc'     => "Barras",
+      'category' => "SO",
+   ),
+   'reportHbarComputersByAge' => array(
+      'title'    => "Computador por idade",
+      'desc'     => "Barras",
+      'category' => "Inventário",
+   ),
+   'reportPieComputersByAge' => array(
+      'title'    => "Computador por idade",
+      'desc'     => "Circular",
+      'category' => "Inventário",
+   ),
+   'reportHbarFusionInventory' => array(
+      'title'    => "Distribuição agente FusionInventory",
+      'desc'     => "Barras",
+      'category' => "Agent",
+   ),
+   'reportPieFusionInventory' => array(
+      'title'    => "Distribuição agente FusionInventory",
+      'desc'     => "Circular",
+      'category' => "Agent",
+   ),
+   'reportHbarMonitors' => array(
+      'title'    => "Distribuição de monitores por computador",
+      'desc'     => "Barras",
+      'category' => "Inventário",
+   ),
+    'reportHbarComputersByEntity' => array(
+      'title'    => "Computadores por entidade",
+      'desc'     => "Barras",
+      'category' => "Entidade",
+    ),
+);
