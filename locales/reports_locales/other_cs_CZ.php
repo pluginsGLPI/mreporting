36341762c114297db74ac70f4d02471386e08a25
p-bo <pavel.borecki@gmail.com>
2018-04-12 07:59:29 +0200

    Added Czech localization for reports - develop branch

---
+++ locales/reports_locales/other_cs_CZ.php
@@ -0,0 +1,12 @@
+<?php
+global $LANG;
+
+$LANG['plugin_mreporting']['Other'] = array(
+   'title' => "Ostatní",
+
+   'reportHbarLogs' => array(
+      'title'    => "Rozložení záznamů událostí",
+      'desc'     => "Pruhový",
+      'category' => "Záznamy událostí (log)",
+   )
+);
