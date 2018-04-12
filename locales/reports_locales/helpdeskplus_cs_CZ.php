d58283da8be33a08d3ff5b9a617df112ffa9f24d
p-bo <pavel.borecki@gmail.com>
2018-04-12 07:59:29 +0200

    Added Czech localization for reports - develop branch

---
+++ locales/reports_locales/helpdeskplus_cs_CZ.php
@@ -0,0 +1,111 @@
+<?php
+global $LANG;
+
+$LANG['plugin_mreporting']['Helpdeskplus'] = array(
+   'title'         => 'Služba podpory – pokročilé',
+
+   // MISC LOCALES
+   'backlogs'      => "Nahromaděná nedodělaná práce",
+   'opened'        => "Otevřené",
+   'period'        => "Období",
+   'backlogstatus' => "Stav který zobrazit",
+   'slaobserved'   => "SLA pozorováno",
+   'slanotobserved'=> "SLA nepozorováno",
+   'observed'      => "pozorováno",
+   'notobserved'   => "nepozorováno",
+
+
+   // SELECTOR
+   'selector'      => array(
+      'slas'       => "SLA smlouvy",
+      'categories' => "Kategorie",
+   ),
+
+
+   // GENERAL REPORTS
+   'reportGlineBacklogs' => array(
+      'title'    => "Nashromážděná nedodělaná práce",
+      'desc'     => "",
+      'category' => "Celkové",
+   ),
+
+   'reportVstackbarLifetime' => array(
+      'title'    => "Stáří požadavku",
+      'desc'     => "",
+      'category' => "Celkové",
+   ),
+
+   'reportVstackbarTicketsgroups' => array(
+      'title'    => "Požadavky podle skupiny",
+      'desc'     => "",
+      'category' => "Celkové",
+   ),
+
+   'reportVstackbarTicketstech' => array(
+      'title'    => "Požadavky podle technika",
+      'desc'     => "",
+      'category' => "Celkové",
+   ),
+
+   'reportVstackbarWorkflow' => array(
+      'title'    => "Orientační fronty",
+      'desc'     => "",
+      'category' => "Celkové",
+   ),
+
+   'reportHbarTopcategory' => array(
+      'title'    => "Nejčastější kategorie",
+      'desc'     => "",
+      'category' => "Celkové",
+   ),
+
+   'reportHbarTopapplicant' => array(
+      'title'    => "Nejčastější žádající skupiny",
+      'desc'     => "",
+      'category' => "Celkové",
+   ),
+
+   'reportVstackbarGroupChange' => array(
+      'title'    => "Počty skupinových změn",
+      'desc'     => "",
+      'category' => "Celkové",
+   ),
+
+   'reportLineActiontimeVsSolvedelay' => array(
+      'title'    => "Porovnání akčních časů úkolu a prodlevy vyřešení",
+      'desc'     => "",
+      'category' => "Celkové",
+   ),
+
+
+   // SLA REPORTS
+   'reportVstackbarNbTicketBySla' => array(
+      'title'    => "Počty požadavků podle SLA",
+      'desc'     => "",
+      'category' => "Podle SLA",
+   ),
+
+   'reportGlineNbTicketBySla' => array(
+      'title'    => "Události v požadavku podle SLA",
+      'desc'     => "",
+      'category' => "Podle SLA",
+   ),
+
+   'reportHgbarRespectedSlasByTopCategory' => array(
+      'title'    => "Požadavky v jednotlivých SLA seřazené podle kategorií",
+      'desc'     => "",
+      'category' => "Podle SLA",
+   ),
+
+   'reportHgbarRespectedSlasByTechnician' => array(
+      'title'    => "Požadavky v jednotlivých SLA seřazené podle techniků",
+      'desc'     => "",
+      'category' => "Podle SLA",
+   ),
+
+   'reportVstackbarRespectedSlasByGroup' => array(
+      'title'    => "Požadavky v jednotlivých SLA seřazené podle skupin",
+      'desc'     => "",
+      'category' => "Podle SLA",
+   ),
+);
---
+++ locales/reports_locales/inventory_cs_CZ.php
@@ -0,0 +1,121 @@
+<?php
+global $LANG;
+
+$LANG['plugin_mreporting']['Inventory'] = array(
+
+   'title' => "Inventura",
+
+   'reportHbarComputersByOS' => array(
+      'title'    => "Počítače podle operačních systémů",
+      'desc'     => "Pruhový",
+      'category' => "Operační systémy",
+   ),
+
+   'reportPieComputersByOS' => array(
+      'title'    => "Počítače podle operačních systémů",
+      'desc'     => "Výsečový",
+      'category' => "Operační systémy",
+   ),
+
+   'reportHbarComputersByFabricant' => array(
+      'title'    => "Počítače podle výrobců",
+      'desc'     => "Pruhový",
+      'category' => "Výrobci",
+   ),
+
+   'reportPieComputersByFabricant' => array(
+      'title'    => "Počítače podle výrobců",
+      'desc'     => "Výsečový",
+      'category' => "Výrobci",
+   ),
+
+   'reportHbarComputersByType' => array(
+      'title'    => "Počítače podle typů",
+      'desc'     => "Pruhový",
+      'category' => "Typy",
+   ),
+
+   'reportPieComputersByType' => array(
+      'title'    => "Počítače podle typů",
+      'desc'     => "Výsečový",
+      'category' => "Typy",
+   ),
+
+   'reportHbarComputersByStatus' => array(
+      'title'    => "Počítač podle stavů",
+      'desc'     => "Pruhový",
+      'category' => "Stavy",
+   ),
+
+   'reportHbarPrintersByStatus' => array(
+      'title'    => "Tiskárny podle stavů",
+      'desc'     => "Pruhový",
+      'category' => "Stavy",
+   ),
+
+   'reportHbarWindows' => array(
+      'title'    => "Verze Windows",
+      'desc'     => "Pruhový",
+      'category' => "Operační systémy",
+   ),
+
+   'reportHbarLinux' => array(
+      'title'    => "Linuxové distribuce podrobně",
+      'desc'     => "Pruhový",
+      'category' => "Operační systémy",
+   ),
+
+   'reportHbarLinuxDistro' => array(
+      'title'    => "Linuxové distribuce podrobně",
+      'desc'     => "Pruhový",
+      'category' => "Operační systémy",
+   ),
+
+   'reportHbarMac' => array(
+      'title'    => "Verze macOS podrobně",
+      'desc'     => "Pruhový",
+      'category' => "Operační systémy",
+   ),
+
+   'reportHbarMacFamily' => array(
+      'title'    => "Přehled verzí macOS",
+      'desc'     => "Pruhový",
+      'category' => "Operační systémy",
+   ),
+
+   'reportHbarComputersByAge' => array(
+      'title'    => "Počítače podle stáří",
+      'desc'     => "Pruhový",
+      'category' => "Inventář",
+   ),
+
+   'reportPieComputersByAge' => array(
+      'title'    => "Počítače podle stáří",
+      'desc'     => "Výsečový",
+      'category' => "Inventář",
+   ),
+
+   'reportHbarFusionInventory' => array(
+      'title'    => "Rozložení verzí FusionInventory agentů",
+      'desc'     => "Pruhový",
+      'category' => "Agenti",
+   ),
+
+   'reportPieFusionInventory' => array(
+      'title'    => "Rozložení verzí FusionInventory agentů",
+      'desc'     => "Výsečový",
+      'category' => "Agenti",
+   ),
+
+   'reportHbarMonitors' => array(
+      'title'    => "Rozložení obrazovek podle počítačů",
+      'desc'     => "Pruhový",
+      'category' => "Inventář",
+   ),
+
+    'reportHbarComputersByEntity' => array(
+      'title'    => "Počítače podle entit",
+      'desc'     => "Pruhový",
+      'category' => "Entity",
+    ),
+);
