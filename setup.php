e52a3467a79dd6b2c66fd25889e652edd6db48a5
Cédric Anne <cedric.anne@gmail.com>
2018-09-04 17:15:13 +0200

    Add 1.5.2 version definition

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
 
-define ('PLUGIN_MREPORTING_VERSION', '1.5.1');
+define ('PLUGIN_MREPORTING_VERSION', '1.5.2');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_MREPORTING_MIN_GLPI", "9.3");
