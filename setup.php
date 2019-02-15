74b26420eb4812ed398f4326fdeec8f28d12fff9
Cédric Anne <cedric.anne@gmail.com>
2019-02-15 11:20:50 +0100

    Add 1.6.1 version definition

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
 
-define ('PLUGIN_MREPORTING_VERSION', '1.6.0');
+define ('PLUGIN_MREPORTING_VERSION', '1.6.1');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_MREPORTING_MIN_GLPI", "9.4");
