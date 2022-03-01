d62ead1246d91202b8fc76cb4534ede8b1707cd5
Cédric Anne <cedric.anne@gmail.com>
2022-03-01 14:34:05 +0100

    Add 1.7.4 version definition

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define ('PLUGIN_MREPORTING_VERSION', '1.7.3');
+define ('PLUGIN_MREPORTING_VERSION', '1.7.4');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_MREPORTING_MIN_GLPI", "9.5");
