68c132a38393bddf714f9c24ac1b224a2d3f18bf
FranPC <bolivarfj@gmail.com>
2022-03-01 14:30:42 +0100

    Add Spanish (Venezuela) locale

---
+++ locales/reports_locales/helpdeskplus_es_VE.php
@@ -0,0 +1,111 @@
+<?php
+global $LANG;
+
+$LANG['plugin_mreporting']['Helpdeskplus'] = [
+   'title'         => 'Soporte avanzado',
+
+   // MISC LOCALES
+   'backlogs'      => "Atrasos",
+   'opened'        => "Abierto",
+   'period'        => "Período",
+   'backlogstatus' => "Estado para mostrar",
+   'slaobserved'   => "ANS supervisado",
+   'slanotobserved'=> "ANS no supervisado",
+   'observed'      => "supervisado",
+   'notobserved'   => "no supervisado",
+
+
+   // SELECTOR
+   'selector'      => [
+      'slas'       => "ANSs",
+      'categories' => "Categorias",
+   ],
+
+
+   // GENERAL REPORTS
+   'reportGlineBacklogs' => [
+      'title'    => "Atraso",
+      'desc'     => "",
+      'category' => "General",
+   ],
+
+   'reportVstackbarLifetime' => [
+      'title'    => "Antiguedad del caso",
+      'desc'     => "",
+      'category' => "General",
+   ],
+
+   'reportVstackbarTicketsgroups' => [
+      'title'    => "Casos por grupo",
+      'desc'     => "",
+      'category' => "General",
+   ],
+
+   'reportVstackbarTicketstech' => [
+      'title'    => "Casos por técnico",
+      'desc'     => "",
+      'category' => "General",
+   ],
+
+   'reportVstackbarWorkflow' => [
+      'title'    => "Orientation queues",
+      'desc'     => "",
+      'category' => "General",
+   ],
+
+   'reportHbarTopcategory' => [
+      'title'    => "TOP categorías",
+      'desc'     => "",
+      'category' => "General",
+   ],
+
+   'reportHbarTopapplicant' => [
+      'title'    => "TOP grupos de solicitantes",
+      'desc'     => "",
+      'category' => "General",
+   ],
+
+   'reportVstackbarGroupChange' => [
+      'title'    => "Caso por numero de cambios de grupos",
+      'desc'     => "Agrupa los casos segun el numero de cambios de grupos",
+      'category' => "General",
+   ],
+
+   'reportLineActiontimeVsSolvedelay' => [
+      'title'    => "Comparativa entre el retrazo de la solución y tiempo de ejecución",
+      'desc'     => "",
+      'category' => "General",
+   ],
+
+
+   // SLA REPORTS
+   'reportVstackbarNbTicketBySla' => [
+      'title'    => "Casos por ANS",
+      'desc'     => "",
+      'category' => "Por ANS",
+   ],
+
+   'reportGlineNbTicketBySla' => [
+      'title'    => "Evolución de casos por ANS",
+      'desc'     => "",
+      'category' => "Por ANS",
+   ],
+
+   'reportHgbarRespectedSlasByTopCategory' => [
+      'title'    => "Casos por ANS ordenados por categorias",
+      'desc'     => "",
+      'category' => "Por ANS",
+   ],
+
+   'reportHgbarRespectedSlasByTechnician' => [
+      'title'    => "Casos por ANS ordenados por técnicos",
+      'desc'     => "",
+      'category' => "Por ANS",
+   ],
+
+   'reportVstackbarRespectedSlasByGroup' => [
+      'title'    => "Casos por ANS ordenados por grupos",
+      'desc'     => "",
+      'category' => "Por ANS",
+   ],
+];
---
+++ locales/reports_locales/other_es_VE.php
@@ -0,0 +1,12 @@
+<?php
+global $LANG;
+
+$LANG['plugin_mreporting']['Other'] = [
+   'title' => "Otro",
+
+   'reportHbarLogs' => [
+      'title'    => "Distribución de eventos",
+      'desc'     => "Barras",
+      'category' => "Eventos",
+   ]
+];
