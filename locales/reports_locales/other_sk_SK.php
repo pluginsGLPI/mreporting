4d0313de69e43ad9fe3de7cd8bc993fe3ff8f574
shoothub <reloadshoot@gmail.com>
2023-11-28 12:44:08 +0100

    Add Slovak locale - translation
    Translated .php reports to Slovak language, please also add .po, .mo from transifex

---
+++ locales/reports_locales/other_sk_SK.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------------
+ * Mreporting plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Mreporting.
+ *
+ * Mreporting is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Mreporting is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mreporting. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ * @copyright Copyright (C) 2003-2023 by Mreporting plugin team.
+ * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/pluginsGLPI/mreporting
+ * -------------------------------------------------------------------------
+ */
+
+global $LANG;
+
+$LANG['plugin_mreporting']['Other'] = [
+   'title' => "Iné",
+
+   'reportHbarLogs' => [
+      'title'    => "Distribúcia logov",
+      'desc'     => "Pruhový",
+      'category' => "Logy",
+   ]
+];
