132a85c715eb87552b35b3a57c657d92a5f8c6a7
Stanislas Kita <skita@teclib.com>
2025-09-30 16:48:47 +0200

    1.8.9

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_MREPORTING_VERSION', '1.8.8');
+define('PLUGIN_MREPORTING_VERSION', '1.8.9');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_MREPORTING_MIN_GLPI', '10.0.11');
