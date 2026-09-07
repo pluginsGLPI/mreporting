db55c17140700d09c3167af863d032ced199635f
Stanislas Kita <7335054+stonebuzz@users.noreply.github.com>
2026-09-07 09:16:30 +0200

    1.8.12

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_MREPORTING_VERSION', '1.8.11');
+define('PLUGIN_MREPORTING_VERSION', '1.8.12');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_MREPORTING_MIN_GLPI', '10.0.11');
